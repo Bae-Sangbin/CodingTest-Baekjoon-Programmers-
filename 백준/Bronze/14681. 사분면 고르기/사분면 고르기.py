824a2f043de613008a6c7a627ee49b334385cc9f
Bae-Sangbin <121172763+Bae-Sangbin@users.noreply.github.com>
2023-02-08 08:26:22 +0900

    [Bronze V] Title: 사분면 고르기, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/14681. 사분면 고르기/사분면 고르기.py
@@ -0,0 +1,10 @@
+data_1 = int(input())
+data_2 = int(input())
+if (data_1 > 0 and data_2 > 0) :
+    print('1')
+elif (data_1 < 0 and data_2 > 0) :
+    print('2')
+elif (data_1 < 0 and data_2 < 0) :
+    print('3')
+else :
+    print('4')
