d78f4b0a1a00ffa50b8883dd12804ce2bcf519f6
Bae-Sangbin <121172763+Bae-Sangbin@users.noreply.github.com>
2023-02-08 09:14:14 +0900

    [level 1] Title: 이름이 있는 동물의 아이디, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/59407. 이름이 있는 동물의 아이디/이름이 있는 동물의 아이디.sql
@@ -0,0 +1,8 @@
+-- SELECT 	필요한 속성만 추출 -> 속성명(컬럼명)
+-- FROM 	어떤 테이블에서? -> 테이블명
+-- WHERE 	조건에 맞는 데이터만 추출 -> 조건
+-- ORDER BY 정렬 기준(ASC, DESC)
+SELECT ANIMAL_ID
+From ANIMAL_INS
+Where NAME is not null
+order by ANIMAL_ID asc
