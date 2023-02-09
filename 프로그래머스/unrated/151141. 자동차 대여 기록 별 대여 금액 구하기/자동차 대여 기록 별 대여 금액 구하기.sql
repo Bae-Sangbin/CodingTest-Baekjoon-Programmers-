990b79bfe09be9d388841eb083d324110819524a
Bae-Sangbin <121172763+Bae-Sangbin@users.noreply.github.com>
2023-02-09 09:37:22 +0900

    [unrated] Title: 자동차 대여 기록 별 대여 금액 구하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/unrated/151141. 자동차 대여 기록 별 대여 금액 구하기/자동차 대여 기록 별 대여 금액 구하기.sql
+++ 프로그래머스/unrated/151141. 자동차 대여 기록 별 대여 금액 구하기/자동차 대여 기록 별 대여 금액 구하기.sql
@@ -9,51 +9,100 @@
 
 # 풀이과정
 
-# SELECT *
-# FROM CAR_RENTAL_COMPANY_CAR A JOIN CAR_RENTAL_COMPANY_RENTAL_HISTORY B ON A.CAR_ID = B.CAR_ID
-# WHERE CAR_TYPE = "트럭"
+# 문제 풀이 방식은 서브쿼리입니다.
+# FROM절의 서브쿼리(인라인 뷰)가 여러 개 중첩된 형태로 이해하시면 됩니다.
 
-# SELECT HISTORY_ID, ROUND((DAILY_FEE - DAILY_FEE * (DISCOUNT_RATE/100))*(DATEDIFF(END_DATE,START_DATE)+1),0) AS FEE
-# FROM
-# (SELECT 
-#     DAILY_FEE,
-#     CAR_TYPE,
-#     B.HISTORY_ID, 
-#     START_DATE,
-#     END_DATE,
+# ① 인라인 뷰 : TBL_1 테이블 만들기
+# SELECT 
 #     A.CAR_ID, 
-# CASE WHEN DATEDIFF(END_DATE,START_DATE)+1 >= 7 THEN '7일 이상'
-#     WHEN DATEDIFF(END_DATE,START_DATE)+1 >= 30 THEN '30일 이상'
-#     WHEN DATEDIFF(END_DATE,START_DATE)+1 >= 90 THEN '90일 이상'
-#     ELSE '7일 미만' END AS 'DURATION_TYPE'
-# FROM CAR_RENTAL_COMPANY_CAR A JOIN CAR_RENTAL_COMPANY_RENTAL_HISTORY B ON A.CAR_ID = B.CAR_ID
-# WHERE CAR_TYPE = "트럭") DIFF JOIN
-# (SELECT *
-# FROM CAR_RENTAL_COMPANY_DISCOUNT_PLAN) DIS ON
-# DIFF.DURATION_TYPE = DIS.DURATION_TYPE
-# ORDER BY FEE DESC, HISTORY_ID DESC;
+#     DAILY_FEE, 
+#     HISTORY_ID, 
+#     START_DATE, 
+#     END_DATE, 
+#     DATEDIFF(END_DATE,START_DATE)+1 AS 'RENTAL_PERIOD'
+# FROM CAR_RENTAL_COMPANY_CAR A JOIN CAR_RENTAL_COMPANY_RENTAL_HISTORY B 
+#     ON A.CAR_ID = B.CAR_ID
+# WHERE CAR_TYPE = "트럭"
+#     ) TBL_1 
+    
+# 인라인 뷰 안에서 CAR_ID를 외래키로 한 두 테이블을 조인한 다음,
+# 자동차 종류가 '트럭'인 자동차만을 WHERE 조건으로 FILTERING 작업한 TBL_1 테이블을 만듭니다.
+# TBL_1이라는 테이블에서 자동차 대여 기간을 의미하는 'RENTAL_PERIOD'도 만듭니다.
 
+# ② 인라인 뷰 : TBL_2 테이블 만들기
+
+# SELECT HISTORY_ID, DAILY_FEE, RENTAL_PERIOD,
+#     CASE WHEN RENTAL_PERIOD >= 90 THEN '90일 이상'
+#          WHEN RENTAL_PERIOD >= 30 THEN '30일 이상'
+#          WHEN RENTAL_PERIOD >= 7 THEN '7일 이상'
+#     ELSE '7일 미만' END AS '구분'
+# FROM(TBL_1)
+
+# TBL_1 테이블을 전체를 감싸고 있는 바깥쪽 쿼리인 TBL_2 테이블을 만듭니다.
+# TBL_2에서는 TBL_1에서 파생적으로 만든 'RENTAL_PERIOD'에 따라서 
+# '90일 이상', '30일 이상', '7일 이상', '7일 미만'으로 구분짓습니다.
+# 이 작업을 하는 이유는 같은 CAR_TYPE이라도 기간의 구간대별로 대여할인율이 달라집니다.
+
+# ③ 인라인 뷰 : TBL_3 테이블 만들기
 
 # SELECT 
-#     CASE WHEN 대여기간 >= 90 THEN '90일 이상'
-#     WHEN 대여기간 >= 30 THEN '30일 이상'
-#     WHEN 대여기간 >= 7 THEN '7일 이상'
-#     ELSE '7일 미만' END AS '구분'
-# FROM(
-# SELECT A.CAR_ID, DAILY_FEE, HISTORY_ID, START_DATE, END_DATE, DATEDIFF(END_DATE,START_DATE)+1 AS '대여기간'
-# FROM CAR_RENTAL_COMPANY_CAR A JOIN CAR_RENTAL_COMPANY_RENTAL_HISTORY B ON A.CAR_ID = B.CAR_ID
-# WHERE CAR_TYPE = "트럭"
-# ORDER BY A.CAR_ID, B.HISTORY_ID
-#     ) TBL
+#     HISTORY_ID, 
+#     DAILY_FEE, 
+#     RENTAL_PERIOD, 
+#     CASE WHEN 구분 = '90일 이상' THEN (SELECT DISCOUNT_RATE
+#                                      FROM CAR_RENTAL_COMPANY_DISCOUNT_PLAN
+#                                      WHERE CAR_TYPE = "트럭"
+#                                      AND DURATION_TYPE = "90일 이상")
+#     WHEN 구분 = '30일 이상' THEN (SELECT DISCOUNT_RATE
+#                                      FROM CAR_RENTAL_COMPANY_DISCOUNT_PLAN
+#                                      WHERE CAR_TYPE = "트럭"
+#                                      AND DURATION_TYPE = "30일 이상")
+#     WHEN 구분 = '7일 이상' THEN (SELECT DISCOUNT_RATE
+#                                      FROM CAR_RENTAL_COMPANY_DISCOUNT_PLAN
+#                                      WHERE CAR_TYPE = "트럭"
+#                                      AND DURATION_TYPE = "7일 이상")
+#     ELSE 0.00 END AS 'DC_RATE'
+# FROM(TBL_2)
+
+# CAR_RENTAL_COMPANY_DISCOUNT_PLAN라는 데이터베이스에 저장된 정보 중
+# 조건(기간의 구간대별로)에 맞게 할인율을 의미하는 'DISCOUNT_RATE'를 서브쿼리를 통해 가져옵니다. 
+
+# ④ 인라인 뷰 : TBL_4 테이블 만들기
+
+# SELECT 
+#     HISTORY_ID, 
+#     DAILY_FEE - (DAILY_FEE * (DC_RATE/100)) AS DISCOUNT_FEE, 
+#     RENTAL_PERIOD
+# FROM (TBL_3)
+
+# 일일 대여 금액(DAILY_FEE)에 할인 적용해서 할인된 새로운 요금을 각행마다 구합니다.
+
+# ⑤ 메인 쿼리
+
+# SELECT 
+#     HISTORY_ID, 
+#     ROUND(RENTAL_PERIOD * DISCOUNT_FEE,0) AS FEE
+# FROM (TBL_4)
+
+# (대여기간 * 할인된 일일 대여 요금)으로 최종 대여금액(FEE)를 구합니다.
+
+# ⑥ 정렬
+
+# ORDER BY 
+#     FEE DESC, HISTORY_ID DESC;
+    
+# 대여 금액을 기준으로 내림차순 정렬하고, 
+# 대여 금액이 같은 경우 대여 기록 ID를 기준으로 내림차순 정렬합니다.
+
 
 #최종 완성된 쿼리
 SELECT 
     HISTORY_ID, 
-    ROUND(RENTAL_PERIOD * 할인적용,0) AS FEE
+    ROUND(RENTAL_PERIOD * DISCOUNT_FEE,0) AS FEE
 FROM (
 SELECT 
     HISTORY_ID, 
-    DAILY_FEE - (DAILY_FEE * (DC_RATE/100)) AS 할인적용, 
+    DAILY_FEE - (DAILY_FEE * (DC_RATE/100)) AS DISCOUNT_FEE, 
     RENTAL_PERIOD
 FROM (
 SELECT 
