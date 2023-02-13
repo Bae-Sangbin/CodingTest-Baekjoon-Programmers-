10c007d6ad8d04c21c87f733949d28fe33388134
Bae-Sangbin <121172763+Bae-Sangbin@users.noreply.github.com>
2023-02-13 15:59:16 +0900

    [unrated] Title: 주문량이 많은 아이스크림들 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/unrated/133027. 주문량이 많은 아이스크림들 조회하기/주문량이 많은 아이스크림들 조회하기.sql
+++ 프로그래머스/unrated/133027. 주문량이 많은 아이스크림들 조회하기/주문량이 많은 아이스크림들 조회하기.sql
@@ -8,12 +8,11 @@
 # 1대다 관계가 됩니다. 다 쪽에 있는 키가 외래키, 1 쪽에 있는 키가 기본키가 됩니다.
 
 
-select j.flavor
-from
-(SELECT flavor, sum(total_order) as total
-from july 
-group by flavor) j join
-(SELECT flavor, sum(total_order) as total
-from FIRST_HALF 
-group by flavor) fh on j.flavor = fh.flavor
-order by fh.TOTAL + j.TOTAL DESC limit 3
+SELECT F_HALF.FLAVOR
+FROM FIRST_HALF F_HALF JOIN 
+(SELECT 
+    DISTINCT FLAVOR, 
+    SUM(TOTAL_ORDER) OVER(PARTITION BY FLAVOR) AS SUM_ORDER
+FROM JULY) JULY_SUM
+ON F_HALF.FLAVOR = JULY_SUM.FLAVOR
+ORDER BY (F_HALF.TOTAL_ORDER + JULY_SUM.SUM_ORDER) DESC LIMIT 3
