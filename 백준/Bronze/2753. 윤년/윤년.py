6d03d568784311ab73de0a158100394af72004f5
Bae-Sangbin <121172763+Bae-Sangbin@users.noreply.github.com>
2023-07-12 23:19:54 +0900

    [Bronze V] Title: 윤년, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/2753. 윤년/윤년.py
+++ 백준/Bronze/2753. 윤년/윤년.py
@@ -1,5 +1,11 @@
-data = int(input())
-if (data % 100 != 0 and data % 4==0) or (data % 400==0) :
-    print(1)
-else :
+n = int(input())
+if n % 4 == 0:
+    if n % 100 != 0:
+        print(1)
+    elif n % 400 == 0:
+        print(1)
+    else :
+        print(0)
+else:
     print(0)
+    
