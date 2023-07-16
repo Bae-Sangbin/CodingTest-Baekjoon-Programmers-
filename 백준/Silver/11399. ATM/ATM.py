d7feea11a39b95e1a7650c8a968d53e1e528b129
Bae-Sangbin <121172763+Bae-Sangbin@users.noreply.github.com>
2023-07-16 13:03:53 +0900

    [Silver IV] Title: ATM, Time: 124 ms, Memory: 114980 KB -BaekjoonHub

--- 백준/Silver/11399. ATM/ATM.py
+++ 백준/Silver/11399. ATM/ATM.py
@@ -2,10 +2,8 @@ N = int(input())
 data = list(map(int, input().split()))
 data.sort()
 
-result = 0
 lst = list()
 for i in range(len(data)):
-    sum_value = sum(data[:i+1])  # data의 0부터 i까지의 요소들의 합을 계산
-    lst.append(sum_value)  # 계산된 합을 lst에 추가
-
-print(sum(lst))  # lst 출력
+    sum_value = sum(data[:i+1])
+    lst.append(sum_value)
+print(sum(lst))
