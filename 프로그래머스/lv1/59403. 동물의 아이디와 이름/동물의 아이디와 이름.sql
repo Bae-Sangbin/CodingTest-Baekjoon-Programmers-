255f81bd069d874e2c9a0558de10cf4dc63d68a3
Bae-Sangbin <121172763+Bae-Sangbin@users.noreply.github.com>
2023-07-17 14:46:48 +0900

    [level 1] Title: 동물의 아이디와 이름, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/lv1/59403. 동물의 아이디와 이름/동물의 아이디와 이름.sql
+++ 프로그래머스/lv1/59403. 동물의 아이디와 이름/동물의 아이디와 이름.sql
@@ -1,7 +1,3 @@
--- SELECT 	필요한 속성만 추출 -> 속성명(컬럼명)
--- FROM 	어떤 테이블에서? -> 테이블명
--- WHERE 	조건에 맞는 데이터만 추출 -> 조건
--- ORDER BY 정렬 기준(ASC, DESC)
+-- 코드를 입력하세요
 SELECT ANIMAL_ID, NAME
 FROM ANIMAL_INS
-ORDER BY ANIMAL_ID;
