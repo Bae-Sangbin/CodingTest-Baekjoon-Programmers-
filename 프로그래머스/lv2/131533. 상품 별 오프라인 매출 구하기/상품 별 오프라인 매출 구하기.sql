49229678a55286a79bbe12973f46f508f6b33400
Bae-Sangbin <121172763+Bae-Sangbin@users.noreply.github.com>
2023-07-20 07:56:05 +0900

    [level 2] Title: 상품 별 오프라인 매출 구하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/lv2/131533. 상품 별 오프라인 매출 구하기/상품 별 오프라인 매출 구하기.sql
+++ 프로그래머스/lv2/131533. 상품 별 오프라인 매출 구하기/상품 별 오프라인 매출 구하기.sql
@@ -1,10 +1,6 @@
-# 6. 집계함수와 윈도우 함수 혼용해서 연습하기
--- PRODUCT 테이블과 OFFLINE_SALE 테이블에서 판매월별 상품코드별로 판매량의 합계(컬럼명: TOTAL_AMOUNT)와 (집계함수 사용)
--- 판매월별로 판매량의 합계를 순위 매긴 리스트를 출력하는 SQL문을 작성해주세요. (DENSE_RANK 사용)
--- 결과는 판매월을 기준으로 1차 정렬해주시고, 판매월이 같다면 TOTAL_AMOUNT를 기준으로 내림차순 정렬해주세요. 
-# (집계함수와 윈도우 함수를 같이 사용해주세요.) 
-
-SELECT PRODUCT_CODE, SUM(PRICE * SALES_AMOUNT) AS 'SALES'
-FROM PRODUCT A JOIN OFFLINE_SALE B ON A.PRODUCT_ID = B.PRODUCT_ID
-GROUP BY PRODUCT_CODE
-ORDER BY SUM(PRICE * SALES_AMOUNT) DESC, PRODUCT_CODE ASC
+-- 코드를 입력하세요
+SELECT P.PRODUCT_CODE, SUM(P.PRICE * O.SALES_AMOUNT) AS 매출액
+FROM PRODUCT P, OFFLINE_SALE O
+WHERE P.PRODUCT_ID = O.PRODUCT_ID
+GROUP BY P.PRODUCT_CODE
+ORDER BY 매출액 DESC, PRODUCT_CODE ASC;
