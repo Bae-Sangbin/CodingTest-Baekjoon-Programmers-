a098951b516cd04c59bd5a356f8103ec1d453416
Bae-Sangbin <121172763+Bae-Sangbin@users.noreply.github.com>
2023-07-20 16:00:13 +0900

    [level 4] Title: 오프라인/온라인 판매 데이터 통합하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/lv4/131537. 오프라인／온라인 판매 데이터 통합하기/오프라인／온라인 판매 데이터 통합하기.sql
+++ 프로그래머스/lv4/131537. 오프라인／온라인 판매 데이터 통합하기/오프라인／온라인 판매 데이터 통합하기.sql
@@ -1,26 +1,28 @@
--- 코드를 입력하세요
-WITH TMP AS(
-SELECT   
-        SALES_DATE
-    ,   PRODUCT_ID
-    ,   USER_ID 
-    ,   SALES_AMOUNT 
-FROM    ONLINE_SALE  
-
-UNION ALL
-
-SELECT  
-        SALES_DATE
-    ,   PRODUCT_ID
-    ,   NULL 
-    ,   SALES_AMOUNT 
-FROM    OFFLINE_SALE  
-)
-SELECT  
-        TO_CHAR(SALES_DATE,'YYYY-MM-DD') SALES_DATE
-    ,   PRODUCT_ID
-    ,   USER_ID 
-    ,   SALES_AMOUNT 
-FROM    TMP 
-WHERE TO_CHAR(SALES_DATE,'YYYYMM') = '202203'
-ORDER BY SALES_DATE, PRODUCT_ID, USER_ID
+SELECT
+    TO_CHAR(SALES_DATE , 'YYYY-MM-DD') AS SALES_DATE,
+    PRODUCT_ID,
+    USER_ID,
+    SALES_AMOUNT
+FROM
+    (
+        SELECT
+            SALES_DATE,
+            PRODUCT_ID,
+            USER_ID,
+            SALES_AMOUNT
+        FROM
+            ONLINE_SALE
+        UNION --합쳐주기
+        SELECT
+            SALES_DATE,
+            PRODUCT_ID,
+            null USER_ID,
+            SALES_AMOUNT
+        FROM
+            OFFLINE_SALE)
+WHERE
+        TO_CHAR(SALES_DATE , 'YYYY-MM') ='2022-03'
+ORDER BY
+    SALES_DATE ASC, --판매일
+    PRODUCT_ID ASC, --상품아이디
+    USER_ID ASC --유저아이디
