58111b1648bcc40a9d2e395ff945bcd256c425d3
Bae-Sangbin <121172763+Bae-Sangbin@users.noreply.github.com>
2023-07-20 13:16:39 +0900

    [unrated] Title: 특정 기간동안 대여 가능한 자동차들의 대여비용 구하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/unrated/157339. 특정 기간동안 대여 가능한 자동차들의 대여비용 구하기/특정 기간동안 대여 가능한 자동차들의 대여비용 구하기.sql
+++ 프로그래머스/unrated/157339. 특정 기간동안 대여 가능한 자동차들의 대여비용 구하기/특정 기간동안 대여 가능한 자동차들의 대여비용 구하기.sql
@@ -1,41 +1,45 @@
-SELECT *
+SELECT DISTINCT *
 FROM (
-    SELECT DISTINCT
-        TBL_2.CAR_ID,
-        TBL_2.CAR_TYPE,
-        30 * ROUND(TBL_2.DAILY_FEE - (TBL_2.DAILY_FEE * TBL_2.DC_RATE)) AS FEE
-    FROM (
-        SELECT 
-            TBL_1.CAR_ID,
-            TBL_1.CAR_TYPE,
-            TBL_1.DAILY_FEE,
-            TBL_1.HISTORY_ID,
-            (
-                SELECT DISCOUNT_RATE/100
-                FROM CAR_RENTAL_COMPANY_DISCOUNT_PLAN
-                WHERE CAR_RENTAL_COMPANY_DISCOUNT_PLAN.CAR_TYPE = TBL_1.CAR_TYPE
-                AND DURATION_TYPE = '30일 이상'
-            ) AS DC_RATE
-        FROM (
-            SELECT 
-                A.CAR_ID,
-                A.CAR_TYPE,
-                A.DAILY_FEE,
-                B.HISTORY_ID
-            FROM CAR_RENTAL_COMPANY_CAR A 
-            JOIN CAR_RENTAL_COMPANY_RENTAL_HISTORY B ON A.CAR_ID = B.CAR_ID
-            WHERE A.CAR_ID NOT IN (
-                SELECT DISTINCT CAR_ID
-                FROM CAR_RENTAL_COMPANY_RENTAL_HISTORY
-                WHERE (
-                    (EXTRACT(MONTH FROM START_DATE) <= 10 AND EXTRACT(YEAR FROM END_DATE) > 2022)
-                    OR (EXTRACT(MONTH FROM START_DATE) <= 10 AND EXTRACT(MONTH FROM END_DATE) >= 11)
-                    OR (EXTRACT(MONTH FROM START_DATE) = 11 AND EXTRACT(MONTH FROM END_DATE) = 11)
-                )
-            )
-            AND A.CAR_TYPE IN ('SUV', '세단')
-        ) TBL_1
-    ) TBL_2
-) TBL_3
-WHERE FEE >= 500000 AND FEE < 2000000
-ORDER BY FEE DESC, CAR_TYPE, CAR_ID DESC;
+SELECT CAR_ID, CAR_TYPE, 30 * ROUND(DAILY_FEE - (DAILY_FEE * DISCOUNT),0) AS FEE
+FROM (
+SELECT 
+    CAR_ID, CAR_TYPE, DAILY_FEE,
+    CASE WHEN CAR_TYPE = '세단' THEN
+    (SELECT DISCOUNT_RATE/100
+     FROM CAR_RENTAL_COMPANY_DISCOUNT_PLAN
+     WHERE DURATION_TYPE = '30일 이상'
+     AND CAR_TYPE = '세단')
+    WHEN CAR_TYPE = 'SUV' THEN
+    (SELECT DISCOUNT_RATE/100
+     FROM CAR_RENTAL_COMPANY_DISCOUNT_PLAN
+     WHERE DURATION_TYPE = '30일 이상'
+     AND CAR_TYPE = 'SUV')
+    END AS DISCOUNT
+FROM (
+    SELECT 
+        C.CAR_ID,
+        C.CAR_TYPE,
+        C.DAILY_FEE,
+        H.START_DATE,
+        H.END_DATE
+    FROM  
+        CAR_RENTAL_COMPANY_CAR C, CAR_RENTAL_COMPANY_RENTAL_HISTORY H
+    WHERE
+        C.CAR_ID = H.CAR_ID
+    AND
+        C.CAR_TYPE IN ('세단', 'SUV')
+    AND 
+        C.CAR_ID NOT IN (SELECT CAR_ID
+                        FROM CAR_RENTAL_COMPANY_RENTAL_HISTORY
+                        WHERE EXTRACT(MONTH FROM START_DATE) <= 10
+                            AND EXTRACT(YEAR FROM END_DATE) > 2022 
+                        OR EXTRACT(MONTH FROM START_DATE) <= 10
+                            AND EXTRACT(MONTH FROM END_DATE) >= 11
+                        OR EXTRACT(MONTH FROM START_DATE) = 11
+                            AND EXTRACT(MONTH FROM END_DATE) = 11)
+        )
+    )
+)
+WHERE FEE BETWEEN 500000 AND 2000000
+ORDER BY 
+    FEE DESC, CAR_TYPE ASC, CAR_ID DESC;
