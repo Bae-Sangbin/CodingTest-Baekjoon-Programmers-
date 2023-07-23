627ce931f65f201f8b5dc1c38684b15619f49eb2
Bae-Sangbin <121172763+Bae-Sangbin@users.noreply.github.com>
2023-07-23 11:07:04 +0900

    [level 0] Title: 한 번만 등장한 문자, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120896. 한 번만 등장한 문자/한 번만 등장한 문자.py
@@ -0,0 +1,55 @@
+def solution(s):
+    answer = ''
+    if s.count('a') == 1:
+        answer = answer + 'a'
+    if s.count('b') == 1:
+        answer = answer + 'b'
+    if s.count('c') == 1:
+        answer = answer + 'c'
+    if s.count('d') == 1:
+        answer = answer + 'd'
+    if s.count('e') == 1:
+        answer = answer + 'e'
+    if s.count('f') == 1:
+        answer = answer + 'f'
+    if s.count('g') == 1:
+        answer = answer + 'g'
+    if s.count('h') == 1:
+        answer = answer + 'h'
+    if s.count('i') == 1:
+        answer = answer + 'i'
+    if s.count('j') == 1:
+        answer = answer + 'j'
+    if s.count('k') == 1:
+        answer = answer + 'k'
+    if s.count('l') == 1:
+        answer = answer + 'l'
+    if s.count('m') == 1:
+        answer = answer + 'm'
+    if s.count('n') == 1:
+        answer = answer + 'n'
+    if s.count('o') == 1:
+        answer = answer + 'o'
+    if s.count('p') == 1:
+        answer = answer + 'p'
+    if s.count('q') == 1:
+        answer = answer + 'q'
+    if s.count('r') == 1:
+        answer = answer + 'r'
+    if s.count('s') == 1:
+        answer = answer + 's'
+    if s.count('t') == 1:
+        answer = answer + 't'
+    if s.count('u') == 1:
+        answer = answer + 'u'
+    if s.count('v') == 1:
+        answer = answer + 'v'
+    if s.count('w') == 1:
+        answer = answer + 'w'
+    if s.count('x') == 1:
+        answer = answer + 'x'
+    if s.count('y') == 1:
+        answer = answer + 'y'
+    if s.count('z') == 1:
+        answer = answer + 'z'
+    return answer
