4905bbc8216d3a6533d3e1e19e2b3bdf7a45008b
Bae-Sangbin <121172763+Bae-Sangbin@users.noreply.github.com>
2023-07-24 17:11:18 +0900

    [unrated] Title: 특정 기간동안 대여 가능한 자동차들의 대여비용 구하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/unrated/157339. 특정 기간동안 대여 가능한 자동차들의 대여비용 구하기/특정 기간동안 대여 가능한 자동차들의 대여비용 구하기.sql
+++ 프로그래머스/unrated/157339. 특정 기간동안 대여 가능한 자동차들의 대여비용 구하기/특정 기간동안 대여 가능한 자동차들의 대여비용 구하기.sql
@@ -1,45 +1,49 @@
-SELECT DISTINCT *
-FROM (
-SELECT CAR_ID, CAR_TYPE, 30 * ROUND(DAILY_FEE - (DAILY_FEE * DISCOUNT),0) AS FEE
-FROM (
-SELECT 
-    CAR_ID, CAR_TYPE, DAILY_FEE,
-    CASE WHEN CAR_TYPE = '세단' THEN
-    (SELECT DISCOUNT_RATE/100
-     FROM CAR_RENTAL_COMPANY_DISCOUNT_PLAN
-     WHERE DURATION_TYPE = '30일 이상'
-     AND CAR_TYPE = '세단')
-    WHEN CAR_TYPE = 'SUV' THEN
-    (SELECT DISCOUNT_RATE/100
-     FROM CAR_RENTAL_COMPANY_DISCOUNT_PLAN
-     WHERE DURATION_TYPE = '30일 이상'
-     AND CAR_TYPE = 'SUV')
-    END AS DISCOUNT
+-- 코드를 입력하세요
+SELECT *
 FROM (
     SELECT 
-        C.CAR_ID,
-        C.CAR_TYPE,
-        C.DAILY_FEE,
-        H.START_DATE,
-        H.END_DATE
-    FROM  
-        CAR_RENTAL_COMPANY_CAR C, CAR_RENTAL_COMPANY_RENTAL_HISTORY H
-    WHERE
-        C.CAR_ID = H.CAR_ID
-    AND
-        C.CAR_TYPE IN ('세단', 'SUV')
-    AND 
-        C.CAR_ID NOT IN (SELECT CAR_ID
-                        FROM CAR_RENTAL_COMPANY_RENTAL_HISTORY
-                        WHERE EXTRACT(MONTH FROM START_DATE) <= 10
-                            AND EXTRACT(YEAR FROM END_DATE) > 2022 
-                        OR EXTRACT(MONTH FROM START_DATE) <= 10
-                            AND EXTRACT(MONTH FROM END_DATE) >= 11
-                        OR EXTRACT(MONTH FROM START_DATE) = 11
-                            AND EXTRACT(MONTH FROM END_DATE) = 11)
-        )
+        DISTINCT
+        CAR_ID,
+        CAR_TYPE,
+        ROUND(30 * (DAILY_FEE - (DAILY_FEE * DC_RATE)), 0) AS FEE
+    FROM 
+        (
+        SELECT 
+            CAR_ID,
+            CAR_TYPE,
+            DAILY_FEE,
+            RENTAL_PERIOD, 
+            CASE WHEN CAR_TYPE = '세단' THEN (SELECT DISCOUNT_RATE / 100
+                                           FROM CAR_RENTAL_COMPANY_DISCOUNT_PLAN
+                                           WHERE CAR_TYPE = '세단'
+                                           AND DURATION_TYPE = '30일 이상')
+                WHEN CAR_TYPE = 'SUV' THEN (SELECT DISCOUNT_RATE / 100
+                                           FROM CAR_RENTAL_COMPANY_DISCOUNT_PLAN
+                                           WHERE CAR_TYPE = 'SUV'
+                                           AND DURATION_TYPE = '30일 이상') 
+                ELSE 0.00 END AS DC_RATE      
+        FROM (
+        SELECT 
+            C.CAR_ID,
+            C.CAR_TYPE,
+            C.DAILY_FEE,
+            H.END_DATE - H.START_DATE + 1 AS RENTAL_PERIOD
+        FROM CAR_RENTAL_COMPANY_CAR C, CAR_RENTAL_COMPANY_RENTAL_HISTORY H
+        WHERE C.CAR_ID = H.CAR_ID
+        AND C.CAR_TYPE IN ('세단', 'SUV')
+        AND C.CAR_ID NOT IN (SELECT CAR_ID
+                            FROM CAR_RENTAL_COMPANY_RENTAL_HISTORY
+                            WHERE 
+                                EXTRACT(MONTH FROM START_DATE) = 11
+                            OR
+                                EXTRACT(MONTH FROM END_DATE) = 11
+                            OR
+                                (EXTRACT(MONTH FROM START_DATE) < 11
+                            AND
+                                TO_CHAR(END_DATE, 'YYYY-MM') > '2022-11')
+                            )
+            )
+        ) 
     )
-)
-WHERE FEE BETWEEN 500000 AND 2000000
-ORDER BY 
-    FEE DESC, CAR_TYPE ASC, CAR_ID DESC;
+WHERE FEE >= 500000 AND FEE < 2000000
+ORDER BY FEE DESC, CAR_TYPE ASC, CAR_ID DESC;
