511e99b85b023bb8256e2eb5de0e9d35d285e32e
Bae-Sangbin <121172763+Bae-Sangbin@users.noreply.github.com>
2023-07-25 23:25:48 +0900

    [unrated] Title: 자동차 대여 기록 별 대여 금액 구하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/unrated/151141. 자동차 대여 기록 별 대여 금액 구하기/자동차 대여 기록 별 대여 금액 구하기.sql
+++ 프로그래머스/unrated/151141. 자동차 대여 기록 별 대여 금액 구하기/자동차 대여 기록 별 대여 금액 구하기.sql
@@ -1,10 +1,10 @@
 -- 코드를 입력하세요
-SELECT HISTORY_ID, 
+SELECT
+    HISTORY_ID,
     ROUND(RENTAL_PERIOD * (DAILY_FEE - (DAILY_FEE * DC_RATE)),0) AS FEE
-FROM 
-(
-    SELECT 
-        CAR_ID, DAILY_FEE, HISTORY_ID, RENTAL_PERIOD, 
+FROM (
+    SELECT
+        CAR_ID, DAILY_FEE, HISTORY_ID, RENTAL_PERIOD,
         CASE WHEN RENTAL_PERIOD >= 90 THEN (SELECT DISCOUNT_RATE / 100
                                            FROM CAR_RENTAL_COMPANY_DISCOUNT_PLAN
                                            WHERE CAR_TYPE = '트럭'
@@ -17,17 +17,13 @@ FROM
                                            FROM CAR_RENTAL_COMPANY_DISCOUNT_PLAN
                                            WHERE CAR_TYPE = '트럭'
                                            AND DURATION_TYPE = '7일 이상')
-            ELSE 0.00 END AS DC_RATE
+        ELSE 0.00 END AS DC_RATE
     FROM (
-    SELECT 
-        C.CAR_ID, 
-        C.DAILY_FEE,
-        H.HISTORY_ID, 
-        H.END_DATE - H.START_DATE + 1 AS RENTAL_PERIOD
+    SELECT C.CAR_ID, C.DAILY_FEE, H.HISTORY_ID,
+        END_DATE - START_DATE + 1 AS RENTAL_PERIOD
     FROM CAR_RENTAL_COMPANY_CAR C, CAR_RENTAL_COMPANY_RENTAL_HISTORY H
     WHERE C.CAR_ID = H.CAR_ID
     AND C.CAR_TYPE = '트럭'
+        )
     )
-)
-ORDER BY 
-    FEE DESC, HISTORY_ID DESC
+ORDER BY FEE DESC, HISTORY_ID DESC;
