26bfa63901117ecf688ce73134a01c6c65b1f8b7
Bae-Sangbin <121172763+Bae-Sangbin@users.noreply.github.com>
2023-08-06 08:37:51 +0900

    [level 0] Title: 특정 문자 제거하기, Time: 0.04 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv0/120826. 특정 문자 제거하기/특정 문자 제거하기.py
+++ 프로그래머스/lv0/120826. 특정 문자 제거하기/특정 문자 제거하기.py
@@ -1,7 +1,9 @@
+import re
 def solution(my_string, letter):
     answer = ''
-    for i in my_string:
-        if i == letter:
-            my_string = my_string.replace(i, '')
-        answer = my_string
+    # for i in my_string:
+    #     if i == letter:
+    #         my_string = my_string.replace(i, '')
+    #     answer = my_string
+    answer = re.sub(letter, '', my_string)
     return answer
