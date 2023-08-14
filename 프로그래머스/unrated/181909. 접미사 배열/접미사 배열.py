66f379ac715fb26d12eacd4635c743c8c2403f1a
Bae-Sangbin <121172763+Bae-Sangbin@users.noreply.github.com>
2023-08-14 13:34:21 +0900

    [unrated] Title: 접미사 배열, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/unrated/181909. 접미사 배열/접미사 배열.py
+++ 프로그래머스/unrated/181909. 접미사 배열/접미사 배열.py
@@ -2,8 +2,7 @@ def solution(my_string):
     answer = []
     my_string = list(my_string)
     for i in range(len(my_string)):
-        temp = my_string
-        temp = ''.join(temp)
+        temp = ''.join(my_string)
         my_string = my_string[1:]
         answer.append(temp)
     return sorted(answer)
