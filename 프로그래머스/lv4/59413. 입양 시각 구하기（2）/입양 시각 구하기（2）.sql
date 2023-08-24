7abadb10c5ca377673ecc74504b609042dbf9ec3
Bae-Sangbin <121172763+Bae-Sangbin@users.noreply.github.com>
2023-08-24 14:00:53 +0900

    [level 4] Title: 입양 시각 구하기(2), Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/lv4/59413. 입양 시각 구하기（2）/입양 시각 구하기（2）.sql
+++ 프로그래머스/lv4/59413. 입양 시각 구하기（2）/입양 시각 구하기（2）.sql
@@ -1,13 +1,19 @@
-SELECT A.HOUR, NVL(B.CNT, 0) AS COUNT
-FROM (
-    SELECT ROWNUM - 1 AS HOUR
-    FROM ANIMAL_OUTS
-    WHERE ROWNUM - 1 < 24
-    ) A,
-    (
-    SELECT TO_CHAR(DATETIME, 'HH24') AS HOUR, COUNT(ANIMAL_ID) AS CNT
-    FROM ANIMAL_OUTS
-    GROUP BY TO_CHAR(DATETIME, 'HH24')
-    ) B
-WHERE A.HOUR = B.HOUR(+)
-ORDER BY A.HOUR
+SELECT
+    HOUR,
+    COUNT(ANIMAL_ID) AS COUNT
+FROM(
+        SELECT H.HOUR, A.ANIMAL_ID
+        FROM (
+                SELECT
+                    ROWNUM - 1 AS HOUR
+                FROM ANIMAL_OUTS
+                WHERE ROWNUM <= 24)  H, 
+                (    
+                    SELECT 
+                        ANIMAL_ID,
+                        TO_CHAR(DATETIME, 'HH24') AS HOUR
+                    FROM ANIMAL_OUTS ) A
+        WHERE H.HOUR = A.HOUR(+)
+    )
+GROUP BY HOUR
+ORDER BY HOUR
