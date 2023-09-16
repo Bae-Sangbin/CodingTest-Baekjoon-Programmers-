b23cd4e278daf9b9fb6cb02b9fbd79276e23d842
Bae-Sangbin <121172763+Bae-Sangbin@users.noreply.github.com>
2023-09-16 20:00:54 +0900

    [unrated] Title: 자동차 대여 기록 별 대여 금액 구하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/unrated/151141. 자동차 대여 기록 별 대여 금액 구하기/자동차 대여 기록 별 대여 금액 구하기.sql
+++ 프로그래머스/unrated/151141. 자동차 대여 기록 별 대여 금액 구하기/자동차 대여 기록 별 대여 금액 구하기.sql
@@ -1,41 +1,28 @@
 SELECT
     HISTORY_ID,
-    ROUND(RENTAL_PERIOD * DISCOUNT_FEE, 0) AS FEE
-FROM(
-    
-    SELECT
-        HISTORY_ID,
-        RENTAL_PERIOD,
-        DAILY_FEE - (DAILY_FEE * (DC_RATE / 100)) AS DISCOUNT_FEE
+    ROUND(RENTAL_PERIOD * (DAILY_FEE - DAILY_FEE * (DC_RATE / 100)), 0) AS FEE
+FROM (
+    SELECT CAR_ID, DAILY_FEE, HISTORY_ID, RENTAL_PERIOD,
+        CASE WHEN RENTAL_PERIOD >= 90 THEN (SELECT DISCOUNT_RATE
+                                           FROM CAR_RENTAL_COMPANY_DISCOUNT_PLAN
+                                           WHERE CAR_TYPE = '트럭'
+                                           AND DURATION_TYPE = '90일 이상')
+            WHEN RENTAL_PERIOD >= 30 THEN (SELECT DISCOUNT_RATE
+                                           FROM CAR_RENTAL_COMPANY_DISCOUNT_PLAN
+                                           WHERE CAR_TYPE = '트럭'
+                                           AND DURATION_TYPE = '30일 이상')
+            WHEN RENTAL_PERIOD >= 7 THEN (SELECT DISCOUNT_RATE
+                                           FROM CAR_RENTAL_COMPANY_DISCOUNT_PLAN
+                                           WHERE CAR_TYPE = '트럭'
+                                           AND DURATION_TYPE = '7일 이상')
+            ELSE 0 END AS DC_RATE
     FROM (
-
-        SELECT
-            HISTORY_ID,
-            DAILY_FEE,
-            RENTAL_PERIOD,
-            CASE
-                WHEN RENTAL_PERIOD >= 90 THEN (SELECT DISCOUNT_RATE
-                                              FROM CAR_RENTAL_COMPANY_DISCOUNT_PLAN
-                                              WHERE CAR_TYPE = '트럭'
-                                              AND DURATION_TYPE = '90일 이상')
-                WHEN RENTAL_PERIOD >= 30 THEN (SELECT DISCOUNT_RATE
-                                              FROM CAR_RENTAL_COMPANY_DISCOUNT_PLAN
-                                              WHERE CAR_TYPE = '트럭'
-                                              AND DURATION_TYPE = '30일 이상')
-                WHEN RENTAL_PERIOD >= 7 THEN (SELECT DISCOUNT_RATE
-                                              FROM CAR_RENTAL_COMPANY_DISCOUNT_PLAN
-                                              WHERE CAR_TYPE = '트럭'
-                                              AND DURATION_TYPE = '7일 이상')     
-                ELSE 0.00
-            END AS DC_RATE
-
-        FROM (
-            SELECT C.CAR_ID, C.DAILY_FEE, H.HISTORY_ID, H.END_DATE - H.START_DATE + 1 RENTAL_PERIOD
-            FROM CAR_RENTAL_COMPANY_CAR C, CAR_RENTAL_COMPANY_RENTAL_HISTORY H
-            WHERE C.CAR_ID = H.CAR_ID
-                AND C.CAR_TYPE='트럭'
-            )
-    )
-)
-ORDER BY 
-    FEE DESC, HISTORY_ID DESC;
+        SELECT 
+            C.CAR_ID, C.DAILY_FEE, H.HISTORY_ID, 
+            DATEDIFF(END_DATE, START_DATE) + 1 AS RENTAL_PERIOD
+        FROM CAR_RENTAL_COMPANY_CAR C JOIN CAR_RENTAL_COMPANY_RENTAL_HISTORY H
+        ON C.CAR_ID = H.CAR_ID
+        WHERE C.CAR_TYPE = '트럭'
+        ) TBL_1
+    ) TBL_2
+ORDER BY FEE DESC, HISTORY_ID DESC;
