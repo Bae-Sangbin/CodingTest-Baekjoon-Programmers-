038aa0fe6a48a39c9c5871cb6630fa34f198182e
Bae-Sangbin <121172763+Bae-Sangbin@users.noreply.github.com>
2023-09-23 21:06:35 +0900

    [unrated] Title: 조건에 맞는 사용자 정보 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/unrated/164670. 조건에 맞는 사용자 정보 조회하기/조건에 맞는 사용자 정보 조회하기.sql
+++ 프로그래머스/unrated/164670. 조건에 맞는 사용자 정보 조회하기/조건에 맞는 사용자 정보 조회하기.sql
@@ -1,25 +1,14 @@
--- SELECT
---     USER_ID, NICKNAME, 
---     (CITY ||' '|| STREET_ADDRESS1 ||' '|| STREET_ADDRESS2) AS "전체주소",
---     (SUBSTR(TLNO, 1, 3)||'-'||SUBSTR(TLNO, 4, 4)||'-'||SUBSTR(TLNO, 8, 4)) AS "전화번호"
--- FROM(
---         SELECT
---             WRITER_ID
---         FROM USED_GOODS_BOARD 
---         GROUP BY WRITER_ID 
---         HAVING COUNT(BOARD_ID) >= 3
---     ) B, USED_GOODS_USER U
--- WHERE B.WRITER_ID = U.USER_ID
--- ORDER BY USER_ID DESC
-
 SELECT
-    USER_ID, 
-    NICKNAME, 
-    CITY ||' '|| STREET_ADDRESS1 ||' '|| STREET_ADDRESS2 AS "전체주소",
-    SUBSTR(TLNO, 1,3)||'-'||SUBSTR(TLNO,4,4)||'-'||SUBSTR(TLNO,8,4) AS "전화번호"
-FROM USED_GOODS_USER
-WHERE USER_ID IN (SELECT WRITER_ID
-                 FROM USED_GOODS_BOARD
-                 GROUP BY WRITER_ID
-                 HAVING COUNT(*) >= 3)
-ORDER BY USER_ID DESC;
+    USER_ID, NICKNAME, 
+    (CITY ||' '|| STREET_ADDRESS1 ||' '|| STREET_ADDRESS2) AS "전체주소",
+    (SUBSTR(TLNO, 1, 3)||'-'||SUBSTR(TLNO, 4, 4)||'-'||SUBSTR(TLNO, 8, 4)) AS "전화번호"
+FROM(
+        SELECT
+            WRITER_ID
+        FROM USED_GOODS_BOARD 
+        GROUP BY WRITER_ID 
+        HAVING COUNT(BOARD_ID) >= 3
+    ) B, USED_GOODS_USER U
+WHERE B.WRITER_ID = U.USER_ID
+ORDER BY USER_ID DESC
+
